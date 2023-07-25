0b42fe80793927db635bcc08a8917201f4778380
trian1220 <trian1220@gmail.com>
2023-07-25 12:36:33 +0700

    commit

---
+++ main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"go-web-native/config"
+	"go-web-native/controllers/categorycontroller"
+	"go-web-native/controllers/homecontroller"
+	"go-web-native/controllers/productcontroller"
+	"log"
+	"net/http"
+)
+
+func main() {
+	config.ConnectDB()
+
+	// 1.home
+	http.HandleFunc("/", homecontroller.Welcome)
+
+	// 2.categories
+	http.HandleFunc("/categories", categorycontroller.Index)
+	http.HandleFunc("/categories/add", categorycontroller.Add)
+	http.HandleFunc("/categories/edit", categorycontroller.Edit)
+	http.HandleFunc("/categories/delete", categorycontroller.Delete)
+
+	// 3. products
+	// 3. Products
+	http.HandleFunc("/products", productcontroller.Index)
+	http.HandleFunc("/products/add", productcontroller.Add)
+	http.HandleFunc("/products/detail", productcontroller.Detail)
+	http.HandleFunc("/products/edit", productcontroller.Edit)
+	http.HandleFunc("/products/delete", productcontroller.Delete)
+
+	log.Println("Server running on port 8080")
+	http.ListenAndServe(":8080", nil)
+}
